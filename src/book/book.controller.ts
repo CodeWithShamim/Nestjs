762b9ed43d0f485796d66165e0d30cd283241e4e
codewithshamim <shamimislamonline@gmail.com>
2023-08-22 13:05:00 +0600

    middleware added

--- src/book/book.controller.ts
+++ src/book/book.controller.ts
@@ -10,4 +10,6 @@ export class BookController {
   getBooks(): string {
     return this.bookService.getBooks();
   }
+
+  
 }
--- src/book/book.module.ts
+++ src/book/book.module.ts
@@ -1,11 +1,16 @@
 /* eslint-disable prettier/prettier */
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { BookController } from './book.controller';
 import { BookService } from './book.service';
+import { LoggerMiddleware } from 'src/middlewares/Logger.middleware';
 
 @Module({
   imports: [],
   controllers: [BookController],
   providers: [BookService],
 })
-export class BookModule {}
+export class BookModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes('book');
+  }
+}
